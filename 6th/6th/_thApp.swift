33b2083bc22694f81834c7a39ba3922a68ec6f10
ssh6lq <154342847+ssh6lq@users.noreply.github.com>
2024-02-02 20:02:21 +0900

    Add files via upload

---
+++ 6th/6th/_thApp.swift
@@ -0,0 +1,17 @@
+//
+//  _thApp.swift
+//  6th
+//
+//  Created by usg on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct _thApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
